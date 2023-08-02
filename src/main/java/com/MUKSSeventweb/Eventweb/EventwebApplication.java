71a31b897f60adb0288fe5cb50e7c796a106601b
Minsung Kang <saimij@naver.com>
2023-08-02 07:01:12 +0100

    Initial Commit

---
+++ src/main/java/com/MUKSSeventweb/Eventweb/EventwebApplication.java
@@ -0,0 +1,13 @@
+package com.MUKSSeventweb.Eventweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventwebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MUKSSeventweb/Eventweb/EventwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MUKSSeventweb.Eventweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
