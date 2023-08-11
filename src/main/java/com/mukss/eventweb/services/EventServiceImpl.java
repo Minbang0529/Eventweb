1eaadeecc9af021ab3a47ba45a0c1cb9621ecf99
dain2 <dain2@LAPTOP-1UQOHG5T>
2023-08-11 14:32:49 +0100

    Fix error in service impl files

--- src/main/java/com/mukss/eventweb/services/EventServiceImpl.java
+++ src/main/java/com/mukss/eventweb/services/EventServiceImpl.java
@@ -3,10 +3,12 @@ package com.mukss.eventweb.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.mukss.eventweb.entities.Event;
 import com.mukss.eventweb.repositories.EventRepository;
 
+@Service
 public class EventServiceImpl implements EventService{
 	
 	@Autowired
