35e83b944af254e1eb478d0737264c152eec54d0
Minsung Kang <saimij@naver.com>
2023-09-19 18:30:35 +0200

    DB Purge test again

--- src/main/java/com/mukss/eventweb/config/initialLoader/InitialDataLoader.java
+++ src/main/java/com/mukss/eventweb/config/initialLoader/InitialDataLoader.java
@@ -37,50 +37,50 @@ public class InitialDataLoader {
 	CommandLineRunner initDatabase() {
 		return args ->{
 			// populate db with users
-			if(userService.count() > 0) {
-				log.info("Database is already populated with users.");
-			}else {
-				
-				Role r1 = new Role();
-				r1.setName("ADMIN");
-				
-				Role r2 = new Role();
-				r2.setName("USER");
-				
-				Role r3 = new Role();
-				r3.setName("MEMBER");
-				
-				roleService.save(r1);
-				roleService.save(r2);
-				roleService.save(r3);
-				
-				User user = new User();
-				user.getRoles().add(r1);
-				user.getRoles().add(r2);
-				user.getRoles().add(r3);
-				user.setFirstName("Minsung");
-				user.setLastName("Kang");
-				// very cheeky, william
-				user.setUserName("prof-kang");
-				user.setPassword(this.passwordEncoder.encode("i-am-a-professor"));
-				user.setEmail("sample@email.com");
-				user.setEnabled(true);
-				user.setMembership("Waiting");
-				
-				User user2 = new User();
-				user2.getRoles().add(r3);
-				user2.setFirstName("william");
-				user2.setLastName("aung");
-				user2.setUserName("aaa");
-				user2.setPassword(this.passwordEncoder.encode("aaa"));
-				user2.setEmail("w.a@gmail.com");
-				user2.setEnabled(true);
-				user2.setMembership("Waiting");
-				
-				
-				userService.save(user);
-				userService.save(user2);
-			}
+//			if(userService.count() > 0) {
+//				log.info("Database is already populated with users.");
+//			}else {
+//				
+//				Role r1 = new Role();
+//				r1.setName("ADMIN");
+//				
+//				Role r2 = new Role();
+//				r2.setName("USER");
+//				
+//				Role r3 = new Role();
+//				r3.setName("MEMBER");
+//				
+//				roleService.save(r1);
+//				roleService.save(r2);
+//				roleService.save(r3);
+//				
+//				User user = new User();
+//				user.getRoles().add(r1);
+//				user.getRoles().add(r2);
+//				user.getRoles().add(r3);
+//				user.setFirstName("Minsung");
+//				user.setLastName("Kang");
+//				// very cheeky, william
+//				user.setUserName("prof-kang");
+//				user.setPassword(this.passwordEncoder.encode("i-am-a-professor"));
+//				user.setEmail("sample@email.com");
+//				user.setEnabled(true);
+//				user.setMembership("Waiting");
+//				
+//				User user2 = new User();
+//				user2.getRoles().add(r3);
+//				user2.setFirstName("william");
+//				user2.setLastName("aung");
+//				user2.setUserName("aaa");
+//				user2.setPassword(this.passwordEncoder.encode("aaa"));
+//				user2.setEmail("w.a@gmail.com");
+//				user2.setEnabled(true);
+//				user2.setMembership("Waiting");
+//				
+//				
+//				userService.save(user);
+//				userService.save(user2);
+//			}
 			// populate db with categories
 			// TODO: Implement categories and initialize here
 			
